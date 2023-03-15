dc49d5649b3292ea93ab47fdac4cf5642bad06ef
Phanindra-Sangers <105136741+Phanindra-Sangers@users.noreply.github.com>
2023-03-15 19:45:16 +0530

    Delete clari5-eks-cluster directory

--- clari5-eks-cluster/backend.tf
+++
@@ -1,9 +0,0 @@
-############# S3 Backend to store state file centrally ####################
-# terraform {
-#   backend "s3" {
-#     bucket          = "clari5-dev-env"
-#     key             = "terraform-tf-state"
-#     region          = "ap-south-1"
-#     dynamodb_table  = "terraform-state-lock"
-#   }
-# }
